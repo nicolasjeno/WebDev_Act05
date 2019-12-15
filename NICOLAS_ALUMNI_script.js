f1069effb2e5d544b6a0937929d789278a306327
nicolasjeno <58454996+nicolasjeno@users.noreply.github.com>
2019-12-14 19:42:44 -0800

    Add files via upload

---
+++ NICOLAS_ALUMNI_script.js
@@ -0,0 +1,21 @@
+function openNav(evt, cityName) {
+  var i, tabcontent, tablinks;
+
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
